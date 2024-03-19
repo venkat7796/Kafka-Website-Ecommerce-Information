535d49dc50925d1f8b32954c9e3193452167f092
venkat7796 <venkatraghavendra7796@gmail.com>
2024-03-19 14:45:10 -0700

    Added Producer and Consumer Logic

---
+++ websiteapp/server/kafkaConsumer/consumer.py
@@ -0,0 +1,49 @@
+from kafka import KafkaConsumer
+from json import dumps, loads
+import json
+import boto3
+import os
+from datetime import datetime
+from dotenv import load_dotenv
+
+def consume_messages_kafka():
+    load_dotenv()
+    aws_ec2 = os.getenv("AWS_EC2")
+    userInfo = KafkaConsumer(
+        'ecommerce_customers',
+        bootstrap_servers=[aws_ec2],
+        value_deserializer=lambda x: loads(x.decode('utf-8'))
+    )
+
+    aws_access_key= os.getenv("AWS_ACCESS_KEY")
+    aws_secret_key= os.getenv("AWS_SECRET_KEY")
+    aws_region = os.getenv("AWS_REGION")
+    aws_bucket = os.getenv("AWS_BUCKET")
+
+    s3 = boto3.client(
+        's3',
+        aws_access_key_id=aws_access_key,
+        aws_secret_access_key=aws_secret_key,
+        region_name=aws_region
+    )
+    print(aws_bucket)
+    for count, values in enumerate(userInfo):
+        print(values.value)
+        json_data = json.dumps(values.value)
+        file_name = "ecommerce_customers_" + str(count) + "_" + str(datetime.now().date())
+        s3.put_object(Body=json_data,Bucket=aws_bucket,Key=f'ecommerce_customers/{file_name}.json')
+
+    productInfo = KafkaConsumer(
+        'ecommerce_products',
+        bootstrap_servers=['18.209.22.67:9092'],
+        value_deserializer=lambda x: loads(x.decode('utf-8'))
+    )
+
+    for count, values in enumerate(productInfo):
+        print(values.value)
+        json_data = json.dumps(values.value)
+        file_name = "ecommerce_products_" + str(count) + "_" + str(datetime.now().date())
+        s3.put_object(Body=json_data,Bucket=aws_bucket,Key=f'ecommerce_products/{file_name}.json')
+
+if __name__ == "__main__":
+    consume_messages_kafka()
--- websiteapp/server/kafkaProducer/producer.py
+++ websiteapp/server/kafkaProducer/producer.py
@@ -1,10 +1,29 @@
 from dotenv import load_dotenv
+from kafka import KafkaProducer
+import json
+from json import dumps
+import os
+
 
 load_dotenv()
 
+def kafkaProduce(topic,message):
+    aws_ec2 = os.getenv("AWS_EC2")
+    producer = KafkaProducer(bootstrap_servers=[aws_ec2],
+                            value_serializer=lambda x: json.dumps(x).encode('utf-8'))
+    if topic == "user_info":
+        payload = {'ecommerce_customers': message}
+        producer.send('ecommerce_customers',value=payload)
+        producer.flush()
+    elif topic == "product_info":
+        payload = {'ecommerce_products': message}
+        producer.send('ecommerce_products',value=payload)
+        producer.flush()
+
 def consume_user_info(message):
-    print(message)
+    kafkaProduce("user_info",message)
     
 
 def consumer_product_info(message):
-    print(message)
+    kafkaProduce("product_info",message)
+
